6618858c3ddc1b8627dd0ec34155c34a4272a20e
jacfger <ncfung@ust.hk>
2023-02-04 00:14:13 +0800

    broken, tex is gged

--- lua/coexistence/extra/sandwich.lua
+++ lua/coexistence/extra/sandwich.lua
@@ -1,6 +1,29 @@
 
 local M = {}
 
+local recipes = {}
+local function add_recipes(recipes_)
+  vim.list_extend(recipes, recipes_)
+  vim.g["sandwich#recipes"] = recipes
+end
+M.add_recipes = add_recipes
+function M.add_recipe(recipe)
+  add_recipes { recipe }
+end
+
+local function add_local_recipes(recipes_)
+  local localrecipes = vim.b.sandwich_recipes
+  if localrecipes == nil then
+    localrecipes = vim.deepcopy(recipes)
+  end
+  vim.list_extend(localrecipes, recipes_)
+  vim.b.sandwich_recipes = localrecipes
+end
+M.add_local_recipes = add_local_recipes
+function M.add_local_recipe(recipe)
+  add_local_recipes { recipe }
+end
+
 function M.setup()
       -- vim.cmd [[
       --     onoremap <Plug>(sandwich-line-helper) :normal! ^vg_<CR>
